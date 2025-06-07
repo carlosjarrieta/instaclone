201f1b32fc4b10a1780e2ce968e20c9f9fcf2e6b
Carlos Arrieta <carlosj.arrieta@gmail.com>
2025-06-07 09:26:04 -0500

    Add semantic ui and sass

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css'
+import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
