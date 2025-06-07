cee69b0eeedfcd6a15a94abcf447dc00021cde85
Carlos Arrieta <carlosj.arrieta@gmail.com>
2025-06-07 09:58:58 -0500

    Add Apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import {Button} from 'semantic-ui-react';
+import {ApolloProvider} from '@apollo/client';
+import client from './config/apollo';
 
 export default function App() {
-  return (
-    <div className="app">
-      <h1>Estamos en App</h1>
-      <div>
-        <Button primary>Primary</Button>
-        <Button secondary>Secondary</Button>
-      </div>
-    </div>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<div className="app">
+				<h1>Estamos en App</h1>
+				<div>
+					<Button primary>Primary</Button>
+					<Button secondary>Secondary</Button>
+				</div>
+			</div>
+		</ApolloProvider>
+	);
 }
