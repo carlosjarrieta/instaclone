b2911170d8aa974f44efc72daf98bcfdea92e07e
Carlos Arrieta <carlosj.arrieta@gmail.com>
2025-06-11 11:35:53 -0500

    Pintando la pagina de auth

--- client/src/pages/Auth/Auth.js
+++ client/src/pages/Auth/Auth.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Auth.scss"
+import {Container, Image} from "semantic-ui-react";
+import instacloneLogo from "../../assets/images/instaclone.png"
 
 export default function Auth() {
-	  return (
-	<div className="auth">
-	  <h1>Estamos en Auth</h1>
-	  <p>Esta es la página de autenticación</p>
-	</div>
-  );
+	const [showLogin, setShowLogin] = useState(true)
+	return (
+		<Container fluid className="auth">
+			<Image src={instacloneLogo} alt="Instaclone"/>
+			<div className="container-form">
+				{showLogin ? <p>Formulario de login</p>: <p>Regístrate</p>}
+			</div>
+
+			<div className="change-form">
+				{showLogin ? (
+					<>
+						¿No tienes una cuenta?
+						<span onClick={() => setShowLogin(!showLogin)}>Regístrate</span>
+					</>
+				) : (
+					<>
+						¿Ya tienes una cuenta?
+						<span onClick={() => setShowLogin(!showLogin)}>Inicia sesión</span>
+					</>
+				)}
+			</div>
+		</Container>
+	);
 }
