c26934e65bab942c055f3f2f0fef5930e5d8c0b8
Carlos Arrieta <carlosj.arrieta@gmail.com>
2025-06-12 08:19:26 -0500

    Control de datos del form con Formik

--- client/src/components/Auth/RegisterForm/RegisterForm.js
+++ client/src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import {Form, Button} from 'semantic-ui-react';
+import {useFormik} from 'formik';
 import './RegisterForm.scss';
 
 export default function RegisterForm(props) {
 	const {setShowLogin} = props;
+	const formik = useFormik({
+		initialValues: initialValues(),
+		validationSchema: null,
+		onSubmit: (formValues) => {
+			console.log("Form submitted formik");
+			console.log(formValues);
+		}
+	});
 	const onSubmit = () => {
 		console.log("Form submitted");
 	}
@@ -13,16 +22,27 @@ export default function RegisterForm(props) {
 			<h2 className='register-form-title'>
 				Regístrate para ver fotos y vídeos de tus amigos.
 			</h2>
-			<Form className='register-form' onSubmit={onSubmit}>
-				<Form.Input type="text" placeholder='Nombre completo' name='name'/>
-				<Form.Input type="text" placeholder='Nombre de usuario' name='username'/>
-				<Form.Input type="text" placeholder='Correo electronico' name='email'/>
-				<Form.Input type="password" placeholder='Contraseña' name='password'/>
-				<Form.Input type="password" placeholder='Repetir Contraseña' name='password_confirm'/>
+			<Form className='register-form' onSubmit={formik.handleSubmit}>
+				<Form.Input type="text" placeholder='Nombre completo' name='name' onChange={formik.handleChange}/>
+				<Form.Input type="text" placeholder='Nombre de usuario' name='username' onChange={formik.handleChange}/>
+				<Form.Input type="text" placeholder='Correo electronico' name='email' onChange={formik.handleChange}/>
+				<Form.Input type="password" placeholder='Contraseña' name='password' onChange={formik.handleChange}/>
+				<Form.Input type="password" placeholder='Repetir Contraseña' name='password_confirm'
+				            onChange={formik.handleChange}/>
 				<Button className='btn-submit' type='submit'>
 					Registrarse
 				</Button>
 			</Form>
 		</>
 	)
+}
+
+function initialValues() {
+	return {
+		name: '',
+		username: '',
+		email: '',
+		password: '',
+		password_confirm: ''
+	}
 }
