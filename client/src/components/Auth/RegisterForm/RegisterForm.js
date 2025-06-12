a7d294201cc06101d95645534d324888615bcd9d
Carlos Arrieta <carlosj.arrieta@gmail.com>
2025-06-12 09:20:47 -0500

    Validando el formulario con Yup

--- client/src/components/Auth/RegisterForm/RegisterForm.js
+++ client/src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import {Form, Button} from 'semantic-ui-react';
 import {useFormik} from 'formik';
+import * as Yup from 'yup';
 import './RegisterForm.scss';
 
 export default function RegisterForm(props) {
 	const {setShowLogin} = props;
 	const formik = useFormik({
 		initialValues: initialValues(),
-		validationSchema: null,
+		validationSchema: validationSchema(),
 		onSubmit: (formValues) => {
-			console.log("Form submitted formik");
 			console.log(formValues);
 		}
 	});
-	const onSubmit = () => {
-		console.log("Form submitted");
-	}
 
 	return (
 		<>
@@ -25,19 +22,29 @@ export default function RegisterForm(props) {
 			<Form className='register-form' onSubmit={formik.handleSubmit}>
 				<Form.Input type="text" placeholder='Nombre completo' name='name'
 				            value={formik.values.name}
-				            onChange={formik.handleChange}/>
+				            onChange={formik.handleChange}
+				            error={formik.errors.name && true}
+				/>
 				<Form.Input type="text" placeholder='Nombre de usuario' name='username'
 				            value={formik.values.username}
-				            onChange={formik.handleChange}/>
+				            onChange={formik.handleChange}
+				            error={formik.errors.username && true}
+				/>
 				<Form.Input type="text" placeholder='Correo electronico' name='email'
 				            value={formik.values.email}
-				            onChange={formik.handleChange}/>
+				            onChange={formik.handleChange}
+				            error={formik.errors.email && true}
+				/>
 				<Form.Input type="password" placeholder='Contraseña' name='password'
 				            value={formik.values.password}
-				            onChange={formik.handleChange}/>
+				            onChange={formik.handleChange}
+				            error={formik.errors.password}
+				/>
 				<Form.Input type="password" placeholder='Repetir Contraseña' name='password_confirm'
 				            value={formik.values.password_confirm}
-				            onChange={formik.handleChange}/>
+				            onChange={formik.handleChange}
+				            error={formik.errors.password_confirm}
+				/>
 				<Button className='btn-submit' type='submit'>
 					Registrarse
 				</Button><
@@ -57,4 +64,21 @@ function initialValues() {
 		password: '',
 		password_confirm: ''
 	}
+}
+
+function validationSchema() {
+	return Yup.object({
+		name: Yup.string().required('El nombre es obligatorio'),
+		username: Yup.string()
+			.matches(/^[a-zA-Z0-9-]*$/, "El nombre del usuario no puede tener espacio")
+			.required('El nombre de usuario es obligatorio'),
+		email: Yup.string().email("El email no es válido").required("El email es obligatorio"),
+		password: Yup.string()
+			.required('La contraseña es obligatoria')
+			.min(6, 'La contraseña debe tener al menos 6 caracteres'),
+		password_confirm: Yup.string()
+			.required('La contraseña es obligatoria')
+			.min(6, 'La contraseña debe tener al menos 6 caracteres')
+			.oneOf([Yup.ref('password')], 'Las contraseñas deben coincidir'),
+	});
 }
