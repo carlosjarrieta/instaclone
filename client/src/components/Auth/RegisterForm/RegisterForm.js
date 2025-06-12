457625fb4bda5125268653e85acfee6b4ff33fb7
Carlos Arrieta <carlosj.arrieta@gmail.com>
2025-06-12 08:35:12 -0500

    Reset del formulario

--- client/src/components/Auth/RegisterForm/RegisterForm.js
+++ client/src/components/Auth/RegisterForm/RegisterForm.js
@@ -23,15 +23,27 @@ export default function RegisterForm(props) {
 				Regístrate para ver fotos y vídeos de tus amigos.
 			</h2>
 			<Form className='register-form' onSubmit={formik.handleSubmit}>
-				<Form.Input type="text" placeholder='Nombre completo' name='name' onChange={formik.handleChange}/>
-				<Form.Input type="text" placeholder='Nombre de usuario' name='username' onChange={formik.handleChange}/>
-				<Form.Input type="text" placeholder='Correo electronico' name='email' onChange={formik.handleChange}/>
-				<Form.Input type="password" placeholder='Contraseña' name='password' onChange={formik.handleChange}/>
+				<Form.Input type="text" placeholder='Nombre completo' name='name'
+				            value={formik.values.name}
+				            onChange={formik.handleChange}/>
+				<Form.Input type="text" placeholder='Nombre de usuario' name='username'
+				            value={formik.values.username}
+				            onChange={formik.handleChange}/>
+				<Form.Input type="text" placeholder='Correo electronico' name='email'
+				            value={formik.values.email}
+				            onChange={formik.handleChange}/>
+				<Form.Input type="password" placeholder='Contraseña' name='password'
+				            value={formik.values.password}
+				            onChange={formik.handleChange}/>
 				<Form.Input type="password" placeholder='Repetir Contraseña' name='password_confirm'
+				            value={formik.values.password_confirm}
 				            onChange={formik.handleChange}/>
 				<Button className='btn-submit' type='submit'>
 					Registrarse
-				</Button>
+				</Button><
+				Button className='btn-reset' type='button' onClick={formik.handleReset}>
+				Reiniciar formulario
+			</Button>
 			</Form>
 		</>
 	)
