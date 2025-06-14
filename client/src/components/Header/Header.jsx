9459cd621cb07bb106180bfb197f8f97fbf2b046
Carlos Arrieta <carlosj.arrieta@gmail.com>
2025-06-14 11:37:06 -0500

    Opciondes del header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import { Container, Grid, Image } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
-import instacloneLogo from '../../assets/images/instaclone.png';
+import {Container, Grid, Image} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+import Logo from '../../assets/images/instaclone.png';
+import RightHeader from "./RightHeader";
 import './Header.scss';
 
 export default function Header() {
-  return (
-    <div className="header">
-      <Container>
-        <Grid>
-          <Grid.Column width={3} className="header__logo">
-            <Link to="/">
-              <Image src={instacloneLogo} alt="Logo" />
-            </Link>
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <p>Buscador</p>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <p>Opciones</p>
-          </Grid.Column>
-        </Grid>
-      </Container>
-    </div>
-  );
+	return (
+		<div className="header">
+			<Container>
+				<Grid>
+					<Grid.Column width={3} className="header__logo">
+						<Link to="/">
+							<Image src={Logo} alt="Logo"/>
+						</Link>
+					</Grid.Column>
+					<Grid.Column width={10}>
+						<p>Buscador</p>
+					</Grid.Column>
+					<Grid.Column width={3}>
+						<RightHeader/>
+					</Grid.Column>
+				</Grid>
+			</Container>
+		</div>
+	);
 }
