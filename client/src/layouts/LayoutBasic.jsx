dac7066dde8eb1af0763d49538a50c1232a417c9
Carlos Arrieta <carlosj.arrieta@gmail.com>
2025-06-14 11:00:54 -0500

    Add Header

--- client/src/layouts/LayoutBasic.jsx
+++ client/src/layouts/LayoutBasic.jsx
@@ -1,14 +1,13 @@
-import React from "react";
-import {Container} from "semantic-ui-react";
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import Header from '../components/Header/Header';
 
 export default function LayoutBasic(props) {
-	const {children} = props;
-	return (
-		<>
-			<h1>HEADER</h1>
-			<Container className="layout-basic">
-				{children}
-			</Container>
-		</>
-	)
+  const { children } = props;
+  return (
+    <>
+      <Header />
+      <Container className="layout-basic">{children}</Container>
+    </>
+  );
 }
