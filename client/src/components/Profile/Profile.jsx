65413548e6894121f34dabf707b0adb6b5c9ab52
Carlos Arrieta <carlosj.arrieta@gmail.com>
2025-06-18 10:31:11 -0500

    Estructura del perfil del usuario

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Grid, Image} from "semantic-ui-react";
 import {useQuery} from "@apollo/client";
 import {GET_USER} from '../../gql/user';
+import ImageNoFound from '../../assets/images/avatar.png';
 import './Profile.scss';
 
 export default function Profile(props) {
@@ -17,7 +19,26 @@ export default function Profile(props) {
 
 	return (
 		<>
-			<h1>Profile</h1>
+			<Grid className='profile'>
+				<Grid.Column width={5} className='profile__left'>
+					<Image avatar src={getUser.avatar || ImageNoFound}/>
+				</Grid.Column>
+				<Grid.Column width={11} className='profile__right'>
+					<div>HeaderProfile</div>
+					<div>Followers</div>
+					<div className="other">
+						<p className='name'>{getUser.name}</p>
+						{getUser.website && (
+							<a href={getUser.website} className='siteWeb' target="_blank" rel="noreferrer">
+								{getUser.website}
+							</a>
+						)}
+						{getUser.bio && (
+							<p className='bio'>{getUser.bio}</p>
+						)}
+					</div>
+				</Grid.Column>
+			</Grid>
 		</>
 	)
 }
--- client/src/gql/user.js
+++ client/src/gql/user.js
@@ -29,6 +29,7 @@ export const GET_USER = gql`
             email
             avatar
             bio
+	        website
         }
     }
 `;
