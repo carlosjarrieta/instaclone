4fdeca8b5ca514bff71f8173abcc1056dae62559
Carlos Arrieta <carlosj.arrieta@gmail.com>
2025-06-19 08:31:43 -0500

    Abriendo buscardor de archivos

--- client/src/components/User/AvatarForm/AvatarForm.jsx
+++ client/src/components/User/AvatarForm/AvatarForm.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {Button} from "semantic-ui-react";
+import {useDropzone} from "react-dropzone";
 import './AvatarForm.scss';
 
 export default function AvatarForm(props) {
 	const {setShowModal} = props;
+
+	const onDrop = useCallback((acceptedFiles) => {
+		// Handle the file upload logic here
+		console.log(acceptedFiles);
+	}, []);
+
+	const {getRootProps, getInputProps} = useDropzone({
+		accept: 'image/*',
+		noKeyboard: true,
+		multiple: true,
+		onDrop,
+	});
+
 	return (
 		<div className='avatar-form'>
-			<Button> Select Image </Button>
+			<Button {...getRootProps()}> Select Image </Button>
 			<Button> Delete Avatar </Button>
 			<Button onClick={() => setShowModal(false)}> Cancel </Button>
+			<input {...getInputProps()} />
 		</div>
 	)
 }
