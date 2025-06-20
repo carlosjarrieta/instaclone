901c7a6496df06d34feae3d2329d382862674094
Carlos Arrieta <carlosj.arrieta@gmail.com>
2025-06-19 19:49:31 -0500

    Funcion para subir imagenes

--- server/utils/aws-upload-image.js
+++ server/utils/aws-upload-image.js
@@ -11,3 +11,21 @@ const s3 = new SDK.S3({
 	accessKeyId: ID,
 	secretAccessKey: SECRET,
 });
+
+async function awsUploadImage(file, filePath) {
+	const params = {
+		Bucket: BUCKET_NAME,
+		Key: filePath,
+		Body: file
+	}
+
+	try {
+		const data = await s3.upload(params).promise();
+		return data.Location;
+	} catch (error) {
+		console.log('Error uploading image to S3:', error);
+		throw new Error('Failed to upload image');
+	}
+}
+
+module.exports = awsUploadImage;
